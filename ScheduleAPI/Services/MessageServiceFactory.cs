434673b67e81cc5fb9e2886745e29076d248f760
Charles Chen <Charles_chen@paradise-soft.com.tw>
2025-03-09 17:59:00 +0800

    refactor: simplify MessageServiceFactory constructor and improve connection string handling

--- ScheduleAPI/Services/MessageServiceFactory.cs
+++ ScheduleAPI/Services/MessageServiceFactory.cs
@@ -5,40 +5,12 @@ namespace ScheduleAPI.Services
     /// <summary>
     /// 消息服務工廠，用於根據配置創建適當的消息服務實例
     /// </summary>
-    public class MessageServiceFactory
+    public class MessageServiceFactory(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-
-        public MessageServiceFactory(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        /// <summary>
-        /// 根據配置創建消息服務
-        /// </summary>
-        /// <returns>消息服務實例</returns>
-        public IMessageService CreateMessageService(string? connectionString = null)
-        {
-            // 如果沒有提供連接字符串，則從配置中獲取
-            connectionString ??= _configuration.GetConnectionString("RabbitMQ") ?? "localhost";
-
-            // 根據配置決定使用哪種消息服務
-            var messageServiceType = _configuration.GetValue<string>("MessageService:Type")?.ToLower() ?? "rabbitmq";
-
-            return messageServiceType switch
-            {
-                "rabbitmq" => new RabbitMqMessageService(connectionString),
-                "redis" => new RedisMessageService(connectionString),
-                // 可以在這裡添加其他消息服務類型
-                _ => new RabbitMqMessageService(connectionString) // 默認使用 RabbitMQ
-            };
-        }
-
         /// <summary>
         /// 創建 Redis 消息服務
         /// </summary>
-        /// <param name="connectionString">Redis 連接字符串</param>
+        /// <param name="connectionString">Redis 連接字串</param>
         /// <returns>Redis 消息服務實例</returns>
         public IMessageService CreateRedisMessageService(string connectionString)
         {
@@ -48,7 +20,7 @@ namespace ScheduleAPI.Services
         /// <summary>
         /// 創建 RabbitMQ 消息服務
         /// </summary>
-        /// <param name="connectionString">RabbitMQ 連接字符串</param>
+        /// <param name="connectionString">RabbitMQ 連接字串</param>
         /// <returns>RabbitMQ 消息服務實例</returns>
         public IMessageService CreateRabbitMQMessageService(string connectionString)
         {
